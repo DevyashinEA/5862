caa67cdd9192144b79fc7fb19b7c766826b553aa
DevyashinEA <55836374+DevyashinEA@users.noreply.github.com>
2019-10-11 01:47:33 +0700

    Добавил бизнеслогику и даже протестировал, но сериализатор не работает

---
+++ NoteApp/NoteApp/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NoteApp
+{
+
+    public class Project { }
+
+    enum CategoryName
+    { Работа, Дом, Здоровье, Люди, Докумнты, Финансы, Разное }
+
+
+    public class Note
+    {
+        private string _name="Без имени";
+        private string _category;
+        private string _text;
+        private DateTime _dateCreate = DateTime.Now;
+        private DateTime _dateChange;
+
+        public string GetName() { return _name; }
+        public void SetName(string name)
+        {
+            if (name.Length > 50)
+            {
+                throw new ArgumentException("Имя мeньше 50 знаков ");
+            }
+            else
+                _name = name;
+            _dateChange = DateTime.Now;
+        }
+
+        public string GetCategory() { return _category; }
+        public void SetCategory(string category)
+        {
+            string[] names = Enum.GetNames(typeof(CategoryName));
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (category == names[i])
+                {
+                    _category = category;
+                    _dateChange = DateTime.Now;
+                }
+                else
+                    if(i == names.Length)
+                        throw new ArgumentException("Категории не существует");
+            }
+        }
+
+        public string GetText() { return _text; }
+        public void SetText(string text)
+        {
+            _text = text;
+            _dateChange = DateTime.Now;
+        }
+
+        public DateTime GetDateCreate() { return _dateCreate; }
+
+        public DateTime GetDateChange() { return _dateChange; }
+    }
+
+    public class ListNote
+    {
+
+    }
+}
---
+++ NoteAppUI/NoteAppUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using NoteApp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace NoteAppUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            var Note = new Note();
+            Note.SetName("One");
+            Note.SetCategory("Дом");
+            Note.SetText("Я делаю лабораторную, но у меня ничего не выходит");
+
+            //Создаём экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer();
+            //Открываем поток для записи в файл с указанием пути
+            using (StreamWriter sw = new StreamWriter(@"D:\vs\DevyashinEA\5862\Notes\Note.txt"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                //Вызываем сериализацию и передаем объект, который хотим сериализовать
+                serializer.Serialize(writer, Note);
+            }
+        }
+    }
+}
